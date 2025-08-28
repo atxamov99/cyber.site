813723f5c5301fe764254b2f19a865a57effd093
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-08-28 19:11:28 +0500

    asdad

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Main from "../components/Main";
+import Section from "../components/Section";
+import Categories from "../components/Categories";
+
+const Home = () => {
+  return (
+    <>
+      <Main />
+      <Categories />
+      <Section />
+    </>
+  );
+};
+
+export default Home;
